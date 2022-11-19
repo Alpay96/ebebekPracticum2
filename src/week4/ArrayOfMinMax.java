5734f7cb007f76f26d677cabd721af0350622a1c
Alpay96 <alpayustalar@hotmail.com>
2022-11-19 15:49:50 +0300

    A program that finds numbers closest to the entered number.

--- src/week4/ArrayOfMinMax.java
+++ src/week4/ArrayOfMinMax.java
@@ -7,11 +7,14 @@ public class ArrayOfMinMax {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        int[] list = {132, 95, 812, -64, -8, 619, 883, 78};
+        int[] list = {132, 95, 612, -64, -8, 419, 883, 78};
 
         System.out.println("Array--> " + Arrays.toString(list));
         System.out.print("Please enter a number to see min/max the nearest: ");
         int val = scan.nextInt();
+        System.out.println();
+        Arrays.sort(list);
+        System.out.println("Sorted array--> " + Arrays.toString(list) + "\tYour number --> " + val);
         minMax(list, val);
     }
 
@@ -25,6 +28,6 @@ public class ArrayOfMinMax {
                 }
             }
         } else
-            System.out.println("Value entered out of range!!");
+            System.out.println("Value entered out of range!");
     }
 }
